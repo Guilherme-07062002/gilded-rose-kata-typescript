847ac4c97c80ef0c357d5aeea243b56b10f6ed76
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-03 09:41:32 -0300

    test: add tests for gilded rose and item classes

--- test/vitest/gilded-rose.spec.ts
+++ test/vitest/gilded-rose.spec.ts
@@ -1,8 +1,39 @@
 import { Item, GildedRose } from "@/gilded-rose";
 
 describe("testing gilded rose", () => {
+  it('should define a object `Item`', () => {
+    const item = new Item("Item", 15, 10);
+
+    expect(item).toBeDefined();
+  })
+
+  it('should define a object `GildedRose`', () => {
+    const gildedRose = new GildedRose();
+
+    expect(gildedRose).toBeDefined();
+  })
+
+  it('should define a object `GildedRose` with a array of items', () => {
+    const item1 = new Item("Item", 15, 10);
+    const item2 = new Item("Item", 15, 10);
+    const gildedRose = new GildedRose([item1, item2]);
+
+    expect(gildedRose.items).toBeDefined();
+    expect(gildedRose.items).toHaveLength(2);
+  })
+
+  it("decreases the `quality` and `sell in` of an item by 1 in the end day", () => {
+    const item = new Item("Item", 15, 10);
+    const gildedRose = new GildedRose([item]);
+
+    const result = gildedRose.updateQuality();
+
+    expect(result[0].sellIn).toBe(14);
+    expect(result[0].quality).toBe(9);
+  });
+
   it("when the item 'sell in' date has passed, the quality decreases twice as fast", () => {
-    const item = new Item("Item", 0, 10);
+    const item = new Item("Item", -1, 10);
     const gildedRose = new GildedRose([item]);
 
     const result = gildedRose.updateQuality();
@@ -11,7 +42,7 @@ describe("testing gilded rose", () => {
   });
 
   it("the quality of an item is never negative", () => {
-    const item = new Item("Item", 0, 0);
+    const item = new Item("Item", 0, -5);
 
     const gildedRose = new GildedRose([item]);
     const result = gildedRose.updateQuality();
@@ -20,16 +51,25 @@ describe("testing gilded rose", () => {
   });
 
   it("the aged brie increase your quality in +1 as it ages", () => {
-    const item = new Item("Aged Brie", 16, 10);
+    const item = new Item("Aged Brie", 16, 49);
 
     const gildedRose = new GildedRose([item]);
     const result = gildedRose.updateQuality();
 
-    expect(result[0].quality).toBe(11);
+    expect(result[0].quality).toBe(50);
   });
 
   it("quality of an item is never higher than 50", () => {
-    const item = new Item("Aged Brie", 0, 50);
+    const item = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49);
+    const gildedRose = new GildedRose([item]);
+
+    const result = gildedRose.updateQuality();
+
+    expect(result[0].quality).toBeLessThanOrEqual(50);
+  });
+
+  it("quality of an item is never higher than 50", () => {
+    const item = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 51);
     const gildedRose = new GildedRose([item]);
 
     const result = gildedRose.updateQuality();
@@ -46,18 +86,16 @@ describe("testing gilded rose", () => {
     expect(result[0].quality).toBeGreaterThanOrEqual(15);
   });
 
-  it("`backstage passes` and `aged brie` increases in quality as sell date approaches", () => {
+  it("`backstage passes` increases +1 in quality as sell date approaches", () => {
     const item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 15);
-    const item2 = new Item("Aged Brie", 15, 15);
-    const gildedRose = new GildedRose([item1, item2]);
+    const gildedRose = new GildedRose([item1]);
 
     const result = gildedRose.updateQuality();
 
     expect(result[0].quality).toBe(16);
-    expect(result[1].quality).toBe(16);
   });
 
-  it("backstage passes and aged brie cheese increases in +2 quality when sell date is lower or equal than 10", () => {
+  it("backstage passes increases +2 in quality when sell date is lower or equal than 10", () => {
     const item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 15);
     const gildedRose = new GildedRose([item]);
 
@@ -66,30 +104,21 @@ describe("testing gilded rose", () => {
     expect(result[0].quality).toBe(17);
   });
 
-  it("backstage passes and aged brie cheese increases in +2 quality when sell date is lower or equal than 10", () => {
-    const item = new Item("Backstage passes to a TAFKAL80ETC concert", 9, 15);
+  it("backstage passes  increases in +3 quality when sell date is lower or equal than 5", () => {
+    const item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 15);
     const gildedRose = new GildedRose([item]);
 
     const result = gildedRose.updateQuality();
 
-    expect(result[0].quality).toBe(17);
+    expect(result[0].quality).toBe(18);
   });
 
-  it("quality of backstage passes is 0 after the concert", () => {
-    const item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 15);
+  it("quality of backstage go to 0 when sell in has passed", () => {
+    const item = new Item("Backstage passes to a TAFKAL80ETC concert", -4, 15);
     const gildedRose = new GildedRose([item]);
 
     const result = gildedRose.updateQuality();
 
     expect(result[0].quality).toBe(0);
-  });
-
-  it("if aged brie sell date is lower than 0 and quality is lower than 50 increase quality +1", () => {
-    const item = new Item("Aged Brie", -10, 40);
-    const gildedRose = new GildedRose([item]);
-
-    const result = gildedRose.updateQuality();
-
-    expect(result[0].quality).toBe(41);
   });
 });
